70fe06275773285f257ec3431965416d0dbe5f85
Gabriel Sobreira <gabriel.sobreira.83@gamil.com>
2025-04-06 10:17:04 -0300

    Questões finalizadas

--- src/Q1/OlaMundo.java
+++ src/Q1/OlaMundo.java
@@ -1,4 +1,9 @@
 package Q1;
 
-public class OlaMundo {
+public class OlaMundo
+{
+    public  static void main(String[] args)
+    {
+        System.out.println("Olá, meu nome é Gabriel e estou aprendendo Java!");
+    }
 }
--- src/Q12/Main.java
+++ src/Q12/Main.java
@@ -8,6 +8,33 @@ public class Main
     {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite o nome do primeiro usuário: ");
+        String usuario1 = sc.nextLine();
 
+
+        System.out.println("Digite o nome do segundo usuário: ");
+        String usuario2 = sc.nextLine();
+
+        String[] mensagens = new String[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            String remetente = (i % 2 == 0) ? usuario1 : usuario2;
+
+            System.out.println(remetente + ", digite sua mensagem: ");
+            String mensagem = sc.nextLine();
+            mensagens[i] = remetente + ": " + mensagem;
+
+
+        }
+
+        System.out.printf("Adeus %s e %s! Obrigado por usar o programa!", usuario1, usuario2);
+
+        System.out.println("\n===== Histórico de Mensagens =====");
+        for (String msg : mensagens)
+        {
+            System.out.println(msg);
+        }
     }
 }
